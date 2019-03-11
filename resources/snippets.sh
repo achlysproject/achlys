af7e0a5b8067d6c3ce903299e4652ae244f0f6d1
Igor Kopestenski <igor.kopestenski@uclouvain.be>
2019-03-11 13:14:28 +0900

    snippet

--- resources/snippets.sh
+++ resources/snippets.sh
@@ -8,3 +8,5 @@ erl -sname rs -remsh achlys@my_grisp_board_1 -setcookie MyCookie -hidden
 erl -sname rs2 -remsh achlys@my_grisp_board_2 -setcookie MyCookie -hidden
 
 networksetup -setmanual "Wi-Fi" 169.254.187.90 255.255.0.0 169.254.187.90
+
+achlys:bite(achlys:declare(rb,all,single,achlys:rainbow())).
